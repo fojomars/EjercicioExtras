3a9def5df0a30c9ccf382418d874e14c1e5cbf61
fojomars <fojomars@victoria130.danielcastelao.org>
2018-11-07 13:24:16 +0100

    Terminando el ejercicio

---
+++ examenes/Examenes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenes;
+
+/**
+ *
+ * @author fojomars
+ */
+public class Examenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NotaMedia alumno = new NotaMedia();
+        alumno.notaTeoricas();
+    }
+    
+}
